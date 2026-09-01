ca34ada0f0c9dfce4c1e3372c55e866d790473b1
surajyellapu <suraj.25bca7752@vitapstudent.ac.in>
2026-09-01 23:07:02 +0530

    Implement basic stack operations in StackExample

---
+++ Stack.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class StackExample {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Stack: " + stack);
+
+        System.out.println("Popped: " + stack.pop());
+
+        System.out.println("Top element: " + stack.peek());
+
+        System.out.println("Stack after pop: " + stack);
+    }
+}
