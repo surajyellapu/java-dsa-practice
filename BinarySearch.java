0a1620276f656901ab7a2c3878d46f142742fee1
surajyellapu <suraj.25bca7752@vitapstudent.ac.in>
2026-09-01 23:03:51 +0530

    Add BinarySearch class with search method
    Implement binary search algorithm in Java.

---
+++ BinarySearch.java
@@ -0,0 +1,33 @@
+public class BinarySearch {
+
+    static int search(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+                return mid;
+
+            if (arr[mid] < key)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int key = 30;
+
+        int result = search(arr, key);
+
+        if (result != -1)
+            System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+}
